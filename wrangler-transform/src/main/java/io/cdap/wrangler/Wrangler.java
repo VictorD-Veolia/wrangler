93eb0abb44abed0c16741c111d843f1b718a24d2
Victor Duvert <victor.duvert.ext@veolia.com>
2021-06-18 15:39:36 +0200

    put back the fix for optional fields in this branch

--- wrangler-transform/src/main/java/io/cdap/wrangler/Wrangler.java
+++ wrangler-transform/src/main/java/io/cdap/wrangler/Wrangler.java
@@ -81,7 +81,7 @@ import javax.annotation.Nullable;
  * by the user.
  */
 @Plugin(type = "transform")
-@Name("Wrangler")
+@Name("DaaS Wrangler")
 @Description("Wrangler - A interactive tool for data cleansing and transformation.")
 public class Wrangler extends Transform<StructuredRecord, StructuredRecord> {
   private static final Logger LOG = LoggerFactory.getLogger(Wrangler.class);
